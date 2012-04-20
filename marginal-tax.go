d84d82e223dfa166d662ff71f476e39f6aa20dcd
Matthew <wonderzombie@gmail.com>
2012-04-19 17:29:50 -0700

    A dumb way to see marginal vs. effective tax rates.
    It probably has a bug somewhere in it, or I've gotten in wrong, but
    it ought to give you a rough idea of how marginal tax rates work.

---
+++ marginal-tax.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type Range struct {
+	Lo	float64
+	Hi	float64
+}
+
+func (r *Range) has(x float64) bool {
+	return x >= r.Lo
+}
+
+type Percentage float64
+
+func (p Percentage) apply(amt float64) float64 {
+	return float64(p) * amt
+}
+
+type Bracket struct {
+	Range
+	Percentage
+}
+
+func NewBracket(lo, hi, p float64) Bracket {
+	return Bracket{Range{lo, hi}, Percentage(p)}
+}
+
+var brackets = []Bracket{
+	NewBracket(0, 8.5e3, 0.10),
+	NewBracket(8.5e3+1, 34.5e3, 0.15),
+	NewBracket(34.5e3+1, 83.6e3, 0.25),
+	NewBracket(83.6e3+1, 174e3, 0.28),
+	NewBracket(174e3+1, 379.15e3, 0.33),
+	NewBracket(379.15e3, 1e10, 0.35),
+}
+
+func main() {
+	income := []float64{9e3, 1e4, 15e3, 2e4, 3e4, 3e5, 3e6, 3e7, 3e9}
+
+	for _, i := range income {
+		var tax float64
+		for _, b := range brackets {
+			if b.Range.has(i) {
+				if i < b.Hi {
+					tax += b.Percentage.apply(i - b.Lo)
+				} else {
+					tax += b.Percentage.apply(b.Hi - b.Lo)
+				}
+			} else {
+				// fmt.Println("Not in bracket:", i, b)
+				break
+			}
+		}
+		fmt.Printf("Tax for %v is %v. Effective rate is %v\n", i, tax, tax/i)
+	}
+}
